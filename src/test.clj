f938d3342a38f1c9216b768ea3f331cb5b76db6b
Joakim Berglund <joakimberglund@live.se>
2016-11-29 16:12:20 +0100

    refactor

--- src/test.clj
+++ src/test.clj
@@ -1,10 +1,10 @@
- (ns test
-   (:use clojure.test))
+(ns test
+  (:use clojure.test))
 
 (defn adder [x y]
   (+ x y))
 
 (deftest test-adder
-         (is (= 2 (adder 1 1))))
+  (is (= 2 (adder 1 1))))
 
 (run-all-tests)
