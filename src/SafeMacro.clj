1f7261d2cfe577d8b93d4d5c26d94c5cd2083975
Joakim Berglund <joakimberglund@live.se>
2016-12-02 10:33:21 +0100

    simple arithmetic working, add test

--- src/SafeMacro.clj
+++ src/SafeMacro.clj
@@ -1,26 +1,26 @@
 (ns SafeMacro
-  (:use clojure.test))
+  (:use clojure.test)
+  (:require clojure.core))
 
 (defmacro safe
   "
   takes a vector containting a variable name and its value as its first argument,
   and an expression as its second argument.
   The macro applies the expression using the variable, and returns the result.
-  If an exception is thrown, the exception is returned instead
+  If an exception is thrown, the exception message is returned instead
   "
   ([expression]
-   ((try
-      (if (vector? (first list))
-        (safe (first list) (rest list))
 
-        )
-      (catch Exception e
-        )))
-   )([vec expression]
-        
-        ))
+   (try (eval expression)
+         (catch Exception e# (str "Exception: " (.getMessage e#)))
+         )
+    )
+  )
 
-(deftest testDivideByZero
-  (is (= "msg" (safe (/ 1 0)))))
+(deftest test-arithmetic-operation
+  (is (= 4 (safe (+ 2 2)))))
+
+(deftest test-divide-by-zero
+  (is (= "Exception: Divide by zero" (safe (/ 2 0)))))
 
 (run-all-tests)
