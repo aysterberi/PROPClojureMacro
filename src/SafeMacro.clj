65f05fa1f57300ddb29dee55937f31becc13d513
Joakim Berglund <joakimberglund@live.se>
2016-12-02 09:17:59 +0100

    begin implementation

--- src/SafeMacro.clj
+++ src/SafeMacro.clj
@@ -1,11 +1,26 @@
-(ns SafeMacro)
+(ns SafeMacro
+  (:use clojure.test))
 
-(defmacro safe-macro
+(defmacro safe
   "
   takes a vector containting a variable name and its value as its first argument,
   and an expression as its second argument.
   The macro applies the expression using the variable, and returns the result.
   If an exception is thrown, the exception is returned instead
   "
-  [variable expression]
-  )
+  ([expression]
+   ((try
+      (if (vector? (first list))
+        (safe (first list) (rest list))
+
+        )
+      (catch Exception e
+        )))
+   )([vec expression]
+        
+        ))
+
+(deftest testDivideByZero
+  (is (= "msg" (safe (/ 1 0)))))
+
+(run-all-tests)
