a21e7a998a00f15074ac12936cf6f03af723fe0e
Joakim Berglund <joakimberglund@live.se>
2016-12-05 15:02:04 +0100

    Names come out, no filtering occurs

--- src/SQLMacro.clj
+++ src/SQLMacro.clj
@@ -1,8 +1,22 @@
 (ns SQLMacro)
 
-(defmacro sql-macro [column table expression orderby]
+(defmacro select [keys _ coll _ cond _ order-cond]
   "
   A macro for searching in lists of maps
   "
-  )
+  `(map (fn [~keys] ~coll)
+        (filter (fn [~keys] ~cond)
+                (sort-by (fn [~keys] ~order-cond)
+                         ~coll))))
+
+(def persons '({:id 1 :name "olle"}
+                {:id 2 :name "anna"}
+                {:id 3 :name "isak"}
+                {:id 4 :name "beatrice"}))
+
+(prn (select [:id :name]
+              from #{persons}
+              where [:id > 1]
+              orderby :name
+              ))
 
