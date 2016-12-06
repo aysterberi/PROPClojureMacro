e5f3d0b5490d5d4be28d8dfbdabdfe2988410cdc
Joakim Berglund <joakimberglund@live.se>
2016-12-06 11:44:36 +0100

    refactor

--- src/SQLMacro.clj
+++ src/SQLMacro.clj
@@ -1,43 +1,35 @@
 (ns SQLMacro
   (:use clojure.test))
 
+;; not operator according to specification
 (defn <> [pre post] (not (= pre post)))
 
-(defmacro select [keys _ coll _ cond _ order-cond]
-  "
-  A macro for searching in lists of maps
-  "
-  ;`(map (fn [~keys] ~coll)
-  ;     (filter (fn [a#] (~@(infix cond))))
-  ;            (sort-by (fn [~keys] ~order-cond)
-  ;                    ~coll)))
-
-  `(map #(select-keys % [~@keys])
-        (filter #(~(second cond) (get % ~(first cond)) ~(last cond))
-                (sort-by #(get % ~order-cond)
-                         ~@coll)))
-  )
-
+;; The map we do our tests on
 (def persons '({:id 1 :name "olle"}
                 {:id 2 :name "anna"}
                 {:id 3 :name "isak"}
                 {:id 4 :name "beatrice"}))
 
+(defmacro select [keys _ coll _ cond _ order-cond]
+  "
+  A macro for searching in lists of maps
+  "
 
+  `(map #(select-keys % [~@keys])
+        ;; We could add another predicate if we wanted
+        (filter #(~(second cond) (get % ~(first cond)) ~(last cond))
+                (sort-by #(get % ~order-cond)
+                         ~@coll)))
 
+  )
+
+;; Something
 (deftest test-greater-than
   (is (= ({:id 4, :name "beatrice"} {:id 3, :name "isak"})
          (select [:id :name]
                  from #{persons}
-                 where (:id > 2)
+                 where [:id > 2]
                  orderby :name))))
-
-;(deftest test-equals
-;  (is (= ({:id 1, :name "olle"})
-;         (select [:id :name]
-;                 from #{persons}
-;                 where (:id < 3)
-;                 orderby :name))))
 
 (run-all-tests)
 
