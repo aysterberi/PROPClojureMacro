a13396d978119dda647b80ab21033f1bc8be4f52
Joakim Berglund <joakimberglund@live.se>
2016-12-06 12:06:27 +0100

    add tests

--- src/SQLMacro.clj
+++ src/SQLMacro.clj
@@ -16,20 +16,39 @@
   "
 
   `(map #(select-keys % [~@keys])
-        ;; We could add another predicate if we wanted
         (filter #(~(second cond) (get % ~(first cond)) ~(last cond))
                 (sort-by #(get % ~order-cond)
                          ~@coll)))
 
   )
 
-;; Something
 (deftest test-greater-than
-  (is (= ({:id 4, :name "beatrice"} {:id 3, :name "isak"})
+  (is (= `({:id 4, :name "beatrice"} {:id 3, :name "isak"})
          (select [:id :name]
                  from #{persons}
                  where [:id > 2]
                  orderby :name))))
+
+(deftest test-less-than
+  (is (= `({:id 1, :name "olle"} {:id 2, :name "anna"})
+         (select [:id :name]
+                 from #{persons}
+                 where [:id < 3]
+                 orderby :id))))
+
+(deftest test-not-equal-to
+  (is (= `({:id 2, :name "anna"} {:id 3, :name "isak"} {:id 4, :name "beatrice"})
+         (select [:id :name]
+                 from #{persons}
+                 where [:id <> 1]
+                 orderby :id))))
+
+(deftest test-equal-to
+  (is (= `({:name "olle", :id 1})
+         (select [:name :id]
+                 from #{persons}
+                 where [:id = 1]
+                 orderby :id))))
 
 (run-all-tests)
 
