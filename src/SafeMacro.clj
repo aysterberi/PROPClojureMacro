4d95910ff006cc969039ebb74cd2479ae6c231df
Joakim Berglund <joakimberglund@live.se>
2016-12-16 12:12:46 +0100

    add vector check

--- src/SafeMacro.clj
+++ src/SafeMacro.clj
@@ -1,3 +1,6 @@
+;; Billy G. J. Beltran(bibe1744) & Joakim Berglund(jobe7147)
+;; Contact details: billy@caudimordax.org, joakimberglund@live.se
+
 (ns SafeMacro
   (:use clojure.test)
   (:require clojure.core)
@@ -12,15 +15,16 @@
   "
   ([expression]
    `(try (~@expression)
-        (catch Exception exception# (str (.toString exception#))))
-    )
+         (catch Exception exception# (str (.toString exception#)))))
   ([vec & expression]
-   ;; From https://clojuredocs.org/clojure.core/with-open:
-   ;; Evaluates body in a try expression with names bound to the values
-   ;; of the inits, and a finally clause that calls (.close name) on each
-   ;; name in reverse order
-   `(try (with-open ~vec ~@expression)
-         (catch Exception exception# (str (.toString exception#))))))
+    ;; From https://clojuredocs.org/clojure.core/with-open:
+    ;; Evaluates body in a try expression with names bound to the values
+    ;; of the inits, and a finally clause that calls (.close name) on each
+    ;; name in reverse order
+   (if (not (vector? vec))
+     "Must be a vector"
+     `(try (with-open ~vec ~@expression)
+           (catch Exception exception# (str (.toString exception#)))))))
 
 (deftest test-arithmetic-operation
   (is (= 4 (safe (+ 2 2)))))
@@ -33,5 +37,8 @@
 
 (deftest test-missing-file
   (is (= "java.io.FileNotFoundException: non-existant (Det går inte att hitta filen)" (safe [s (FileReader. (File. "non-existant"))] (. s read)))))
+
+(deftest test-wrong-input
+  (is (= "Must be a vector" (safe list (s (FileReader. (File. "file.txt"))) (.read s)))))
 
 (run-tests)
