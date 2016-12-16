c78e722c38cdf7300221a3cca5588c67ffb233eb
Joakim Berglund <joakimberglund@live.se>
2016-12-16 12:13:00 +0100

    format, new tests

--- src/SQLMacro.clj
+++ src/SQLMacro.clj
@@ -1,16 +1,24 @@
+;; Billy G. J. Beltran(bibe1744) & Joakim Berglund(jobe7147)
+;; Contact details: billy@caudimordax.org, joakimberglund@live.se
+
 (ns SQLMacro
   (:use clojure.test))
 
 ;; not operator according to specification
 (defn <> [pre post] (not (= pre post)))
 
-;; The map we do our tests on
+;; The maps we do our tests on
 (def persons '({:id 1 :name "olle"}
                 {:id 2 :name "anna"}
                 {:id 3 :name "isak"}
                 {:id 4 :name "beatrice"}))
+(def empty-persons '())
+(def duplicates '({:id 1 :name "olle"}
+                   {:id 1 :name "olle"}
+                   {:id 2 :name "anna"}
+                   {:id 2 :name "anna"}))
 
-(defmacro select [keys _ collection _ where-condition _ order-condition]
+(defmacro select [keys _ collection _ where-condition _ order-argument]
   "
   An sql select-like macro for searching in lists of maps. Takes a vector of [keys] to look up,
   a collection with key/value pairs, a condition to determine which key/value pairs are selected,
@@ -19,12 +27,14 @@
   select [keys]
   from #{collection}
   where [where-condition]
-  orderby order-condition
+  orderby order-argument
   "
   `(map #(select-keys % [~@keys])
-        (filter #(~(second where-condition) (get % ~(first where-condition)) ~(last where-condition))
-                (sort-by #(get % ~order-condition)
-                         ~@collection))))
+        (filter #(~(second where-condition)
+                   (get % ~(first where-condition))
+                   ~(last where-condition))
+                (sort-by #(get % ~order-argument)
+                         (set ~@collection)))))
 
 (deftest test-greater-than
   (is (= `({:id 4, :name "beatrice"} {:id 3, :name "isak"})
@@ -53,6 +63,20 @@
                  from #{persons}
                  where [:id = 1]
                  orderby :id))))
+
+(deftest test-empty-map
+  (is (= `()
+         (select [:name :id]
+                 from #{empty-persons}
+                 where [:id = 1]
+                 orderby :id))))
+
+(deftest test-with-duplicates
+  (is (= `({:name "anna" :id 2} {:name "olle" :id 1})
+         (select [:name :id]
+                 from #{duplicates}
+                 where [:id > 0]
+                 orderby :name))))
 
 (run-tests)
 
